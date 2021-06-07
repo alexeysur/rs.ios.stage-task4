ed0f0a980ba8249e2b133303d8fd215ace74055d
Alexey Sur <alexeysur@gmail.com>
2021-06-07 22:42:46 +0300

    feat: solution for exercices

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,31 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+        var value = self
+        var result = ""
+        let romanKeys: [(number: Int, symbol: String)] = [(1000, "M"),
+                                                          (900, "CM"),
+                                                          (500, "D"),
+                                                          (400, "CD"),
+                                                          (100, "C"),
+                                                          (90, "XC"),
+                                                          (50, "L"),
+                                                          (40, "XL"),
+                                                          (10, "X"),
+                                                          (9, "IX"),
+                                                          (5, "V"),
+                                                          (4, "IV"),
+                                                          (1, "I")]
+        
+        if value <= 0 { return nil }
+        else {
+            for element in romanKeys {
+                while (value >= element.number) {
+                    value -= element.number
+                    result += element.symbol
+                }
+            }
+            return result
+        }
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,36 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        let m = image.count
+        let n = image.first?.count ?? 0
+        
+        if (m == 0 || row >= m || column >= image[0].count || row < 0 || column < 0) {
+            return image
+        }
+        
+        if (m >= 50 && m < 0 && n >= 50 && n <= 1 && newColor <= 0 && newColor > 65536) {
+            return image
+        }
+        
+        var image = image
+        let color = image[row][column]
+        
+        recursiveFill(&image, row, column, color, newColor)
+        return image
+        
+    }
+    
+    fileprivate func recursiveFill(_ image: inout [[Int]], _ row: Int, _ column: Int, _ color: Int, _ newColor: Int) {
+        if row >= image.count ||  column >= image[0].count || row < 0 || column < 0 || image[row][column] != color || image[row][column] == newColor {
+            return
+        }
+        
+        image[row][column] = newColor
+        
+        recursiveFill(&image, row - 1, column, color, newColor)
+        recursiveFill(&image, row + 1, column, color, newColor)
+        recursiveFill(&image, row, column - 1, color, newColor)
+        recursiveFill(&image, row, column + 1, color, newColor)
     }
 }
+
